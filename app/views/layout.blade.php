b61b3d9ae3cf194875f2fc514c4260f56af3dba3
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-10 14:21:31 -0300

    project

---
+++ app/views/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+{{ HTML::style('bootstrap/css/bootstrap.min.css'); }}
+{{ HTML::script('bootstrap/js/bootstrap.min.js'); }}
+
+</head>
+
+<body>
+
+	@yield('content')
+</body>
+
+</html>
---
+++ app/views/posts.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<h1>Latest Posts</h1>
+
+
+@stop
