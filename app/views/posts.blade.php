2a54199570399f97a8ca36cadf357282552cff43
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-10 14:34:00 -0300

    added post view

--- app/views/posts.blade.php
+++ app/views/posts.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <h1>Latest Posts</h1>
-
+<ul class="posts list">
+	@foreach ($posts as $post)
+		<li class="entry">
+			<h2>{{ $post->title }}</h2>
+			<p>{{ $post->content }}</p>
+		
+		</li>
+	@endforeach
+</ul>
 
 @stop
