9b2f93537f5a9f17180c81fb512718d187f9aca3
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-11 11:26:23 -0300

    changing layout format

--- app/routes.php
+++ app/routes.php
@@ -13,4 +13,31 @@
 
 Route::get('/', 'PostsController@showIndex');
 
+Route::get('admin', array( 'uses' => 'DashboardController@showIndex'));
+Route::get('admin/posts/add', 'DashboardController@addPost');
+Route::post('admin/posts/add', 'DashboardController@addPost');
+Route::get('admin/posts/edit/{id}', 'DashboardController@editPost');
 
+Route::get('login', 'UsersController@login'); 
+Route::get('register', 'UsersController@register'); 
+
+Route::post('login/process', 'UsersController@processLogin');
+Route::post('register/process', 'UsersController@processRegister');
+
+Route::get('/404', function(){
+	View::make('notfound');
+});
+
+
+/*
+	Routing pattern
+*/
+
+Route::when('admin/*', 'isLoggedIn');
+
+/*Defining Route Filters*/
+
+Route::filter('isLoggedIn', function(){
+		if(!Auth::check())
+			return Redirect::to('/login');
+});
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,13 +1,30 @@
+<!DOCTYPE html>
 <html>
 <head>
+<title>@yield('title')</title>
+
 {{ HTML::style('bootstrap/css/bootstrap.min.css'); }}
-{{ HTML::script('bootstrap/js/bootstrap.min.js'); }}
+{{ HTML::style('bootstrap/css/bootstrap-theme.min.css'); }}
+
 
 </head>
 
 <body>
-
-	@yield('content')
+	<div class="container">
+	
+	
+		<header class="row" >
+			<!--Renders the Page header define in template as (at)section('header') -->
+			@yield('header')
+		</header>
+		<div class="row">
+			<!--Renders the Page content -->
+			@yield('content')
+		</div>
+	
+	</div>
+	
+	{{ HTML::script('bootstrap/js/bootstrap.min.js'); }}
 </body>
 
 </html>
