d0e51fc348ea16a58f6345cb9cf168cb644b2a67
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-13 20:38:37 -0300

    added category relationships

--- app/models/Post.php
+++ app/models/Post.php
@@ -6,4 +6,13 @@ class Post extends Eloquent {
 	public function comments(){
 		return $this->hasMany('Comment');
 	}
+	
+	public function category(){
+		return $this->belongsTo('Category');
+	}
+	/*
+	*
+	*/	
+
+
 }
