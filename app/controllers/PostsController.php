57c8c779ac08cc2a91a5be4ab0b1fea6511634c5
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-19 11:29:39 -0300

    added pagination

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -7,24 +7,24 @@ class PostsController extends BaseController {
 	public function showIndex($id = null )
 	{
 		if(isset($id)){
-			$posts = Category::find($id)->posts()->take(10)->get();
-			
-			
+			$posts = Category::find($id)->posts()->take(10)->paginate(10);
+
+
 		} else {
-			$posts = Post::where('published', '=', true)->take(10)->get();
+			$posts = Post::where('published', '=', true)->take(10)->paginate(10);
 		}
-		
-		
+
+
 		$categories = Category::all();
-	
+
 		return View::make('frontend.posts', array('posts' => $posts, 'categories' => $categories));
 	}
-	
+
 	public function showPost($id = null){
 		if(isset($id)){
 			$post = Post::find($id);
 			$comments = Post::find($id)->comments;
-			
+
 			return View::make('frontend.post', array('post' => $post, 'comments' => $comments));
 		} else {
 			return Redirect::to('/404');
