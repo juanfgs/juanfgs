7ec6088bc31c59086f567fd50f651c38a3f79d3c
Juan F. Giménez Silva <juanfgs@gmail.com>
2014-05-19 11:29:32 -0300

    added excerpt method for post

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,18 +1,19 @@
 <?php
 
 class Post extends Eloquent {
-		
-	
+
+	public $active;
 	public function comments(){
 		return $this->hasMany('Comment');
 	}
-	
+
 	public function category(){
 		return $this->belongsTo('Category');
 	}
-	/*
-	*
-	*/	
+
+	public function excerpt(){
+		return substr($this->content, 0, 140) . '...';
+	}
 
 
 }
